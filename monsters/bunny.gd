53e48b378cd9f3c4c46a47d2ee6c2391037b276e
Skwint <squirmy@gmail.com>
2023-12-22 10:02:36 +0000

    Bunny movement

--- monsters/bunny.gd
+++ monsters/bunny.gd
@@ -11,10 +11,14 @@ var feet_shape
 var stage : int
 var stage_scales = [0.5, 1.0, 1.5, 2.0]
 
-const fertility : float = 0.1 / 60.0
+const fertility : float = 0.2 / 60.0
+const min_ball_distance : float = 1.0
 const max_ball_distance : float = 3.0
-const max_ball_velocity : float = 1.0
-const ball_thrust : float = 0.1
+const max_ball_velocity : float = 1.5
+const max_speed : float = 1.0
+const ball_thrust : float = 0.05
+const ball_repel : float = 0.03
+const bunny_thrust : float = 0.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -28,6 +32,14 @@ func _process(delta):
 	pass
 	
 func _physics_process(delta):
+	var is_on_floor = false
+	if body.position.y > 0.1:
+		var contacts = body.get_colliding_bodies()
+		for contact in contacts:
+			if contact.is_in_group("floor"):
+				is_on_floor = true
+				break
+	
 	# split
 	if stage == stage_scales.size() - 1:
 		multiply()
@@ -36,18 +48,26 @@ func _physics_process(delta):
 			multiply()
 			
 	# ball thrust
-	if ball.position.distance_squared_to(body.position) < max_ball_distance:
+	var distsq = ball.position.distance_squared_to(body.position)
+	if distsq < min_ball_distance:
+		ball.apply_central_impulse(ball_repel * body.position.direction_to(ball.position))
+	elif distsq < max_ball_distance:
 		if ball.linear_velocity.length_squared() < max_ball_velocity:
 			ball.apply_central_impulse(Vector3(randf_range(-ball_thrust, ball_thrust), 0.0, randf_range(-ball_thrust, ball_thrust)))
 	
-	# turn to face ball
-	body.look_at(ball.position)
-	
-	# drive
+	if is_on_floor:
+		# turn to face ball
+		body.look_at(ball.position)
+		
+		# drive
+		var dir = body.position.direction_to(ball.position)
+		var speed = body.linear_velocity.dot(dir)
+		if speed < max_speed:
+			body.apply_central_impulse(dir * bunny_thrust)
 
 func multiply():
 	var angle = PI / 4
-	var move = Vector3(0.0, 0.0, - stage_scales[stage] * 0.3)
+	var move = Vector3(0.0, 0.0, stage_scales[stage] * 0.3)
 	var base_rotation : Vector3 = body.get_rotation()
 	for b in 4:
 		var inst
@@ -68,15 +88,14 @@ func set_stage(s):
 	var scv = Vector3(sc, sc, sc)
 	model.scale = scv
 	body_shape.scale = scv
+	ball.position.y = body_shape.scale.y * 0.28
 
 func randomize():
 	set_stage(randi_range(0,stage_scales.size() - 2))
 	body.rotate_y(randf_range(0.0, 2.0 * PI))
 
 func reposition(pos):
-	pos.y = max(pos.y, 0.5)
-	
 	body.set_position(pos)
 	var ballpos = pos + Vector3(0.0, 0.0, 0.3).rotated(Vector3(0.0,1.0,0.0), body.get_rotation().y)
-	ballpos.y = pos.y
+	ballpos.y = body_shape.scale.y * 0.28
 	ball.set_position(ballpos)
