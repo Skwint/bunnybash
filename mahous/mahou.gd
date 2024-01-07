4ecfec708902f68c5850aca63e98aff7bcec1b72
Skwint <squirmy@gmail.com>
2024-01-07 17:12:58 +0000

    viable motion for mahous

--- mahous/mahou.gd
+++ mahous/mahou.gd
@@ -17,27 +17,37 @@ func _process(_delta):
 
 
 func _physics_process(_delta):
-	var dx = 0.0
-	var dz = 0.0
-	if Input.is_action_pressed("move_right"):
-		dx += 1.0
-		dz -= 1.0
-	if Input.is_action_pressed("move_left"):
-		dx -= 1.0
-		dz += 1.0
-	if Input.is_action_pressed("move_up"):
-		dx -= 1.0
-		dz -= 1.0
-	if Input.is_action_pressed("move_down"):
-		dx += 1.0
-		dz += 1.0
+	var is_on_floor = false
+	if position.y > 0.1:
+		var contacts = get_colliding_bodies()
+		for contact in contacts:
+			if contact.is_in_group("floor"):
+				is_on_floor = true
+				break
 	
-	if dx != 0.0 and dz != 0.0:
-		dx *= 0.707
-		dz *= 0.707
+	if (is_on_floor):
+		var dx = 0.0
+		var dz = 0.0
+		if Input.is_action_pressed("move_right"):
+			dx += 1.0
+			dz -= 1.0
+		if Input.is_action_pressed("move_left"):
+			dx -= 1.0
+			dz += 1.0
+		if Input.is_action_pressed("move_up"):
+			dx -= 1.0
+			dz -= 1.0
+		if Input.is_action_pressed("move_down"):
+			dx += 1.0
+			dz += 1.0
 		
+		if dx != 0.0 and dz != 0.0:
+			dx *= 0.707
+			dz *= 0.707
+
+		apply_central_force(Vector3(dx * thrust, 0.0, dz * thrust))
 
 
 func spawn(pos):
-	position = Vector3(pos.x + 0.5, 0.5, pos.y + 0.5)
+	position = Vector3(pos.x + 0.5, 0.75, pos.y + 0.5)
 	camera.make_current()
